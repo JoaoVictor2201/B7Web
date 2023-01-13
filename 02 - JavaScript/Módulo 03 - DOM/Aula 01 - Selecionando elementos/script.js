b73303a00651996277b08bf1ef40fb31f2bd7144
João Victor França <franca.jv@hotmail.com>
2023-01-13 18:32:49 -0300

    Modulo 03 - Aula 01

---
+++ 02 - JavaScript/Módulo 03 - DOM/Aula 01 - Selecionando elementos/script.js
@@ -0,0 +1,9 @@
+let element = document.getElementsByTagName('h1'); // Seleciona os elementos que possuam a tag passada como parâmetro
+
+let element_2 = document.getElementById('teste'); // Seleciona o elemento que possui o ID passado no parametro
+
+let element_3 = document.getElementsByClassName('teste'); // Seleciona todos os elementos quie possuem a classe passada no parametro
+
+let element_4 = document.querySelector('.teste') // Seleciona um unico elemento passado pelo parametro em formato de seletor CSS de classe ou id
+
+let element_5 = document.querySelectorAll('.teste') // Seleciona todos elementos passados pelo parametro em formato de seletor CSS de classe ou id
