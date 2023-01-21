f91c22c22c83b4686de85381488584a7eba5beff
João Victor França <franca.jv@hotmail.com>
2023-01-21 12:03:08 -0300

    Modulo 04 - Aula 02

--- 02 - JavaScript/Módulo 04 - Orientação a Objetos/Aula 03 - Classes - Actions/script.js
+++ 02 - JavaScript/Módulo 04 - Orientação a Objetos/Aula 03 - Classes - Actions/script.js
@@ -1,10 +1,30 @@
 class Person {
+
+    steps = 0;
+    age = 0;
+    
     constructor(name, age) {
         this.name = name;
-        this.age = age;
     }
+
+    takeAStep() {
+        this.steps++; // inserção de funções dentro de uma classe
+    }
+
+    setAge(newAge) {
+        if (typeof newAge == 'number') {
+            this.age = newAge
+        }
+    }
+
 }
 
-let pessoa1 = new Person('João', 24); // Instaciamento de classes (Criação de um objeto com os moldes da classe criada)
-let pessoa2 = new Person('Maria', 30);
-let pessoa3 = new Person('Ronaldo', 40);
+let pessoa1 = new Person('João');
+let pessoa2 = new Person('Maria');
+let pessoa3 = new Person('Ronaldo');
+
+pessoa1.takeAStep();
+console.log(`Passos de ${pessoa1.name}: ${pessoa1.steps}`);
+
+pessoa1.setAge(30);
+console.log(`${pessoa1.name} tem ${pessoa1.age} anos`);
