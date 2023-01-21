172968820438e4ff7d7073b6d8451653d89fe733
João Victor França <franca.jv@hotmail.com>
2023-01-21 12:28:22 -0300

    Modulo 04 - Aula 05

--- 02 - JavaScript/Módulo 04 - Orientação a Objetos/Aula 05 - Classes - Herança/script.js
+++ 02 - JavaScript/Módulo 04 - Orientação a Objetos/Aula 05 - Classes - Herança/script.js
@@ -6,4 +6,19 @@ class Person {
         this.name = name;
     }
 
+    dizerOla() {
+        console.log(`${this.name} diz oi!`);
+    }
+
 }
+
+class Student extends Person { // Herdando as informações da classe importada
+    
+    constructor(name, id) {
+        super(name); // Referencia o atributo da classe importada
+        this.id = id;
+    }
+}
+
+let pessoa1 = new Student('João', 1);
+pessoa1.dizerOla();
