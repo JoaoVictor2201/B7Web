36692b8b302d9801e2f06effdee1aeccb4ee2695
João Victor França <franca.jv@hotmail.com>
2023-01-21 11:53:36 -0300

    Modulo 04 - Aula 02

---
+++ 02 - JavaScript/Módulo 04 - Orientação a Objetos/Aula 02 - Classes - Instâncias/script.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let pessoa1 = new Person('João', 24); // Instaciamento de classes (Criação de um objeto com os moldes da classe criada)
+let pessoa2 = new Person('Maria', 30);
+let pessoa3 = new Person('Ronaldo', 40);
