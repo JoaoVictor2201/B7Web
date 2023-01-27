535c532263c0b045471f09acb0a759710fd8ff70
João Victor França <franca.jv@hotmail.com>
2023-01-27 20:36:42 -0300

    Módulo 05 - Aula 03

---
+++ 02 - JavaScript/Módulo 05 - Requisições/Aula 03 - Como funciona uma API/script.js
@@ -0,0 +1,15 @@
+// API = Application Programming Interface
+/*
+    Uma API serve para fazer a comunicação ente dois usuários,
+    sejam eles um navegador, um software, etc. Qualquer coisa
+    que precise que certos dados sejam processador de uma certa
+    maneira e enviados de volta para serem exibidos de uma certa
+    forma, e para isso, precisamos de uma "linguagem universal"
+    para as API's conseguirem fazer essa comunicação, no caso do
+    JS temos o JSON
+
+    JSON = JavaScript Object Notation
+
+    https://jsonplaceholder.typicode.com/posts
+    Site de API fake para testar seus códigos
+*/
