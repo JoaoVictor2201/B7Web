5ac040ab7a4c93fd5580248c493b38a0ce2a9e33
João Victor França <franca.jv@hotmail.com>
2023-01-27 20:21:56 -0300

    Módulo 05 - Aula 02

---
+++ 02 - JavaScript/Módulo 05 - Requisições/Aula 02 - Req e Resp/script.js
@@ -0,0 +1,12 @@
+// Request e Response
+/* 
+    Request = Requisição
+    -> Cabeçalhos / Headers - Informações que ajudam o servidor a saber como tratar os dados pedidos (de onde vem a requisição, qual o tipo do aparelho, qual o navegador usado, cookies, etc...)
+    -> Corpo da requisição / Body - Informações Brutas (Mensagens digitadas, uma pesquisa feita dentro do site), podem ser vazios no caso de requisições apenas para exibir uma página que não precisa de informações do usuário
+*/
+
+/*
+    Response = Resposta
+    -> Cabeçalhos / Headers - Informações mandadas pelo servidor (Cache)
+    -> Corpo / Body - Código HTML para exibição da página
+*/
