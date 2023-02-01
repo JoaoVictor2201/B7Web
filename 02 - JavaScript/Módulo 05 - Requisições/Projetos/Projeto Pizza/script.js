0fca76138449e5b316183c145eb5d50b3ca54b9b
João Victor França <franca.jv@hotmail.com>
2023-02-01 14:09:57 -0300

    Módulo 05 - Projeto Pizza 0.3

--- 02 - JavaScript/Módulo 05 - Requisições/Projetos/Projeto Pizza/script.js
+++ 02 - JavaScript/Módulo 05 - Requisições/Projetos/Projeto Pizza/script.js
@@ -4,12 +4,30 @@ const selectAll = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
     let pizzaItem = select('.models .pizza-item').cloneNode(true);
     
+    pizzaItem.setAttribute('data-pizzaKey', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        let pizzaKey = e.target.closest('.pizza-item').getAttribute('data-pizzaKey');
+        let pizzaQtd = 1;
+        
+        select('.pizzaBig img').src = pizzaJson[pizzaKey].img;
+        select('.pizzaInfo h1').innerHTML = pizzaJson[pizzaKey].name;
+        select('.pizzaInfo--desc').innerHTML = pizzaJson[pizzaKey].description;
+        select('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[pizzaKey].price.toFixed(2)}`;
+        select('.pizzaInfo--size.selected').classList.remove('selected');
+        selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[pizzaKey].sizes[sizeIndex];
+        });
+
+        select('.pizzaInfo--qt').innerHTML = pizzaQtd;
 
         select('.pizzaWindowArea').style.opacity = 0;
         select('.pizzaWindowArea').style.display = 'flex';
